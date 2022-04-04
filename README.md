7d664778e53318b10dd5a35d493967830277ee88
Nick McMillen <91640914+nmcmillen@users.noreply.github.com>
2022-04-04 14:30:01 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+### Project Setup
+
+NOTE: We do not do the dice rolling, rolling of the dice "happens".
+
+1. Double check that you have the following extensions installed:
+- AREPL for python.
+- Python Test Explorer for VSCode
+
---
+++ index.py
@@ -0,0 +1,25 @@
+from connection import create_connection
+
+connection = create_connection("./test_db.db")
+cursor = connection.cursor()
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+create_users_table = """
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL,
+  age INTEGER,
+  gender TEXT,
+  nationality TEXT
+);
+"""
+
+execute_query(connection, create_users_table)
